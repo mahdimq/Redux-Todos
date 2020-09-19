fda9460099dadec2485c23a8a5cc26824753f180
Muhammad Qadir <mahdimq@gmail.com>
2020-09-19 13:35:17 +0300

    edit form

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -3,7 +3,14 @@ import React, { useState } from 'react';
 function TodoForm({ newTodo }) {
 	const [item, setItem] = useState('');
 
+	// const handleChange = (e) => {
+	// 	const { name, value } = e.target;
+	// 	console.log(name, value);
+	// 	setItem((todo) => ({ ...todo, [name]: value }));
+	// };
+
 	const handleChange = (e) => setItem(e.target.value);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		newTodo(item);
@@ -14,7 +21,8 @@ function TodoForm({ newTodo }) {
 		<div>
 			<form onSubmit={handleSubmit}>
 				<label htmlFor='todo'>Enter Todo:</label>
-				<input id='todo' type='text' onChange={handleChange} value={item} />
+				{/* <input id='todo' type='text' onChange={handleChange} value={item.name} /> */}
+				<input id='todo' type='text' onChange={handleChange} />
 				<button type='submit'>Add!</button>
 			</form>
 		</div>
